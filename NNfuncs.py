e6ed76eacea0a04e013a22c2e97d65e7ac802c0c
AlessandroFumi <alessandro.fum@gmail.com>
2017-06-15 12:51:53 +0200

    Add files via upload
    This is a (non working) version of NNSim with custom convolutional operators. The custom operators are needed to emulate the behaviour of operations such as convolution when implemented with analogic circuitry instead with microprocessors.
    
    The file "NNetSim_class.py" will be used to reformulate the code in terms of python classes rather than

--- NNfuncs.py
+++ NNfuncs.py
@@ -9,143 +9,261 @@ Initialization of the NNetSim
 #from skimage import io
 
 import numpy as np
-from scipy import ndimage, misc
 
-import NNvars
+""" MISC FUNCTIONS NEEDED """
+#def checking():
+#    # All this checking is done because this function is really buggy
+#    if np.mod(input_size-kernel_size,stride) or kernel_size > input_size or stride > input_size:
+#        print("The input, kernel and stride size have to satisfy certain relations.")
+#        exit(1)
+#    elif stride > kernel_size:
+#        print("We're not yet ready for dilated convolutions.")
+#        exit(1)
+#    return isgood
+
+def threeDtofiveDconv(tensor, reps, sorting_indexes):
+    num_slidings = reps.sum()
+    # Replicate and ordering rows and columns
+    tensor = np.repeat(tensor,reps,axis=-1)
+    tensor = tensor[...,sorting_indexes]
+    tensor= np.repeat(tensor,reps,axis=-2)
+    tensor = tensor[...,sorting_indexes,:]
+    # Splitting in equal parts
+    tensor= np.array(np.split(tensor,num_slidings,axis=-1))
+    tensor= np.array(np.split(tensor,num_slidings,axis=-2))
+    return tensor
+
+def gen_indexes(input_size,kernel_size,stride):
+    # Total number of slidings
+    # We could devise this function to be called just at the beginning
+    num_slidings = int((input_size-kernel_size) / stride + 1)
+    out_indexes = np.array([])
+    for i in np.arange(num_slidings):
+        out_indexes = np.append(out_indexes,np.arange(i*stride ,i*stride+kernel_size))
+    
+    sorting_indexes = out_indexes.argsort().argsort()
+    (ignore,reps) =  np.unique(out_indexes, return_counts = True)
+
+    return num_slidings, reps, sorting_indexes
+
+def fast3dconv(up_neurons,out_size,weights,stride):
+    # Extracting sizes
+    input_size = up_neurons.shape[-1]
+    kernel_size = weights.shape[-1]
+    # Padding the upstream neurons
+    pad_width = int(( (out_size-1) * stride + kernel_size - input_size) / 2)
+    npad = ((0,0),(pad_width,pad_width),(pad_width,pad_width))
+    # Extracting sizes
+    input_size += 2*pad_width
+    # We have two different versions here
+    num_slidings, reps, sorting_indexes = gen_indexes(input_size,kernel_size,stride)
+    # Padding
+    up_neurons = np.pad(up_neurons, npad, 'constant', constant_values = 0)
+    # Replicate and ordering rows and columns
+    up_neurons = np.repeat(up_neurons,reps,axis=-1)
+    up_neurons = up_neurons[...,sorting_indexes]
+    up_neurons= np.repeat(up_neurons,reps,axis=-2)
+    up_neurons = up_neurons[...,sorting_indexes,:]
+    # Splitting in equal parts
+    up_neurons= np.array(np.split(up_neurons,num_slidings,axis=-1))
+    up_neurons= np.array(np.split(up_neurons,num_slidings,axis=-2))
+
+    #Replicating weights
+    weights = np.tile(weights,(num_slidings,num_slidings))
+    weights = np.array(np.split(weights,num_slidings,axis=-1))
+    weights = np.array(np.split(weights,num_slidings,axis=-2))
+    #Here we have the assumption that weights are a 4D tensor turned 6D
+    weights = weights.transpose([2,0,1,3,4,5])
+    
+    # Einstein product: convolution without biasing
+    down_neurons = np.einsum('...ijk,...ijk->...', up_neurons , weights)
+    
+    return down_neurons
+
+def fiveDtothreeD(tensor):
+    tensor = tensor.transpose([1,3,2,4,0])
+    tensor = np.concatenate(tensor, axis = 0)
+    tensor = tensor.transpose([1,2,3,0])
+    tensor = np.concatenate(tensor, axis = 0)
+    tensor = tensor.transpose([1,2,0])
+    return tensor
+"""END OF IMPLEMENTATION WITH 5D TENSORS"""
+
+"""IMPLEMENTATION OF CONVOLUTION WITH IM2COL AND FANCY INDEXING"""
+def get_im2col_indices(x_shape, field_height, field_width, padding=1, stride=1):
+  # First figure out what the size of the output should be
+  C, H, W = x_shape
+  assert (H + 2 * padding - field_height) % stride == 0
+  assert (W + 2 * padding - field_width) % stride == 0
+  out_height = int( (H + 2 * padding - field_height) / stride + 1 )
+  out_width = int( (W + 2 * padding - field_width) / stride + 1 )
+
+  i0 = np.repeat(np.arange(field_height), field_width)
+  i0 = np.tile(i0, C)
+  i1 = stride * np.repeat(np.arange(out_height), out_width)
+  j0 = np.tile(np.arange(field_width), field_height * C)
+  j1 = stride * np.tile(np.arange(out_width), out_height)
+  i = i0.reshape(-1, 1) + i1.reshape(1, -1)
+  j = j0.reshape(-1, 1) + j1.reshape(1, -1)
+
+  k = np.repeat(np.arange(C), field_height * field_width).reshape(-1, 1)
+
+  return (k, i, j)
+
+def im2col_indices(x, field_height, field_width, padding=1, stride=1):
+  """ An implementation of im2col based on some fancy indexing """
+  # Zero-pad the input
+  p = padding
+  x_padded = np.pad(x, ((0, 0), (p, p), (p, p)), mode='constant')
+
+  k, i, j = get_im2col_indices(x.shape, field_height, field_width, padding,
+                               stride)
+
+  cols = x_padded[k, i, j]
+  C = x.shape[0]
+  cols = cols.reshape(field_height * field_width * C, -1)
+  return cols
+
+def col2im_indices(cols, x_shape, field_height=3, field_width=3, padding=1,
+                   stride=1):
+  """ An implementation of col2im based on fancy indexing and np.add.at """
+  C, H, W = x_shape
+  H_padded, W_padded = H + 2 * padding, W + 2 * padding
+  x_padded = np.zeros((C, H_padded, W_padded), dtype=cols.dtype)
+  k, i, j = get_im2col_indices(x_shape, field_height, field_width, padding,
+                               stride)
+  cols_reshaped = cols.reshape(C * field_height * field_width, -1)
+  np.add.at(x_padded, (k, i, j), cols_reshaped)
+  if padding == 0:
+    return x_padded
+  return x_padded[:, :, padding:-padding, padding:-padding]
+
+def im2col_conv(x_in,x_out,filters,biases,stride=1,padding=1):
+    field_height, field_width = filters.shape[-2:]
+    # Convert x to matrix
+    in_cols = im2col_indices(x_in, field_height, field_width, padding, stride)
+    filters = filters.reshape(filters.shape[0],-1)
+    out_cols = np.dot(filters,in_cols)+biases[...,None]
+    return col2im_indices(out_cols, x_out.shape,
+                          field_height, field_width, padding, stride)
+
+"""END OF IMPLEMENTATION OF CONVOLUTION WITH IM2COL"""
+
 """NEURON ACTIVATION FUNCTIONS AND DERIVATIVE!"""
-def net_input(weights, biases, upstream_values,normalization_values):
-    # Returns the same array with values comprised between -1 and 1, with 
-    # linear normalization
-    return np.divide(np.add(biases.ravel(),np.dot(upstream_values,weights)),
-              normalization_values.sum()+1)
-
-def ReLU_activation(neuron_values):
-    return neuron_values.clip(min=0)
+def ReLU_activation(net_input_array):
+    return net_input_array.clip(min=0)
 
 def ReLU_derivative(neuron_values):
     return (neuron_values > 0.0).astype(np.float)
 
-def sigmoid_activation(net_input_array):
-    # Returns the element-wise calculation of the sigmoid activation function
-    return 1/(1 + np.exp(np.multiply(-NNvars.slope_sigmoid,net_input_array)))
+"""LAYER FUNCTIONALITY, FORWARD"""
+def fc_layer(up_neurons, weights, biases):
+    # We use the dot product to calculate the net input of every neuron,
+    # and subsequently add the bias vector
+    down_neurons = np.add(biases.ravel(),
+                           np.dot(up_neurons,weights))
+    return down_neurons
 
-def sigmoid_derivative(neuron_values):
-    # Returns the element-wise calculation of the sigmoid derivative starting
-    # from the activation value (NOT THE NET INPUT!)
-    return np.multiply(neuron_values,np.subtract(1,neuron_values))
+def ReLU_layer(up_neurons):
+    # Normalization + 'clip' routine that simulates the ReLU behaviour
+    down_neurons = ReLU_activation(up_neurons)
+    return down_neurons
 
-def tanh_activation(net_input_array):
-    # Returns the element-wise calculation of the tanh activation function
-    return 0.5*np.tanh(np.multiply(NNvars.slope_tanh,net_input_array))+0.5
+def conv_layer(up_neurons,down_neurons,weights,biases,stride):
+    out_size = down_neurons.shape[-1]
+    # Do the convolution and add the biases
+    down_neurons = fast3dconv(up_neurons,out_size,weights,stride) + biases[...,None,None]
+    return down_neurons
 
-def tanh_derivative(neuron_values):
-    # Returns the element-wise calculation of the tanh derivative
-    return np.subtract(1,np.power(neuron_values,2))
+def maxpool_layer(up_neurons,down_neurons):
+    # Extracting sizes
+    out_size = down_neurons.shape[-1]
+    # Divide the input volume slices by NxN using the filter dimension
+    up_neurons = np.array(np.split(up_neurons,out_size,axis=-1))
+    up_neurons = np.array(np.split(up_neurons,out_size,axis=-2))
+    # Pool the maximum from correct axes and reorder
+    down_neurons = up_neurons.max(axis=(3,4)).transpose([2,0,1])
+    # Build boolean mask for error propagation
+    up_neurons = up_neurons.transpose([2,0,1,3,4])
+    bool_mask = (up_neurons == down_neurons[...,None,None]).astype(np.int)
+    # If we want to reproduce the size of input layer, to be checked
+#    bool_mask = fiveDtothreeD(bool_mask)
+    return down_neurons,bool_mask
 
-def PWL_activation(net_input_array):
-    # Returns the element-wise calculation of the tanh activation function
-    return np.multiply(NNvars.slope_PWL,net_input_array).clip(min=0,max=1)
 
-def PWL_derivative(neuron_values):
-    # Returns the element-wise calculation of the tanh derivative
-    return np.logical_and(neuron_values > 0.0,neuron_values < 1.0).astype(np.float)
+"""LAYER FUNCTIONALITY, BACK"""
+def out_delta(output_values, output_deltas, label):
+    # Pass the new delta values to the heavyweight part of the program
+    # To check the correct working principle for batch size != 1
+    output_deltas.fill(0)
+    ground_truth = np.zeros(len(output_deltas))
+    ground_truth[label] = max(output_values)
+    output_deltas += np.subtract(ground_truth, output_values)
+    
+    return output_deltas
 
-"""FULLY CONNECTED NEURAL NETWORKS FUNCTIONALITY"""
+def fc_layer_back(down_errors, weights):
+    up_errors = np.dot(down_errors, weights.transpose())
+    return up_errors
+
+def ReLU_layer_back(down_errors,up_neurons):
+    # Normalization + 'clip' routine that simulates the ReLU behaviour
+    up_errors = np.multiply(down_errors,ReLU_derivative(up_neurons))
+    return up_errors
+
+def conv_layer_back(up_errors,down_errors,weights):
+    weights = np.flip(weights,axis=-1)
+    weights = np.flip(weights,axis=-2)
+    # The backpropagation of a convolutional layer is still a convolution
+    up_errors = fast3dconv(down_errors,up_errors, weights)
+    return up_errors
+
+def maxpool_layer_back(up_errors,down_errors,bool_mask):
+    up_errors = np.multiply(down_errors[...,None,None], bool_mask)
+    up_errors = fiveDtothreeD(up_errors)
+    return up_errors
+
+"""WEIGHT UPDATES"""
+def fc_layer_wu(up_neurons,down_errors,learningRate):
+    weight_change = np.multiply(learningRate, np.outer(up_neurons,down_errors))
+    bias_change = np.multiply(learningRate, down_errors) 
+    return weight_change, bias_change
+
+def conv_layer_wu(up_neurons, down_errors, learningRate):
+    
+    weight_change = np.array(0)
+    bias_change = np.array(0)
+    return weight_change, bias_change
+"""DATA FLOW CONTROLLERS"""
 def input_layer(input_neurons, image):
     # Input layer
     # The variable input_neurons is usually denoted as neuron_values[0] while
     # image is denoted with data_in[1]
     
     # Normalized
-    input_neurons = np.divide(image.ravel(), image.ravel().max())
+    input_neurons = image.ravel()
     
     return input_neurons
 
 def forward_inference(weights, biases, neuron_values):
-    for thisLayer in range(len(neuron_values)-1):
-        # We use the dot product to calculate the net input of every neuron,
-        # and subsequently add the bias vector
-        norm_input = net_input(weights[thisLayer], biases[thisLayer],
-                               neuron_values[thisLayer],neuron_values[thisLayer])
-        # Normalization + 'clip' routine that simulates the ReLU behaviour
-        neuron_values[thisLayer+1] += activation_function(norm_input)
+    # Here we'll put the control for the forward evaluation
     
     return neuron_values
 
-def out_delta(output_values, output_deltas, label):
-    # Pass the new delta values to the heavyweight part of the program
-    # To check the correct working principle for batch size != 1
-    output_deltas.fill(0)
-    ground_truth = np.zeros(len(output_deltas))
-    ground_truth[label] = 1
-    output_deltas += np.multiply(neuron_derivative(output_values),np.subtract(ground_truth, output_values))
-    
-    return output_deltas
       
-def backpropagation_MSE(weights, neuron_values, delta_values):
-    # Backpropagation
-    for thisLayer in range(1,len(neuron_values)-1):
-        # Normalized with neuron values derivatives
-        norm_input = np.multiply(neuron_derivative(neuron_values[-thisLayer-1]),
-                                 net_input(weights[-thisLayer].transpose(),np.zeros(1),
-                                           delta_values[-thisLayer],np.zeros(1))
-                                 )
-        delta_values[-thisLayer-1] += norm_input
-    
+def backpropagation(weights, neuron_values, delta_values):
+    # Controller for backpropagation
     return delta_values
-
-def backpropagation_xentropy(weights, neuron_values, delta_values):
-    # Backpropagation
-    for thisLayer in range(1,len(neuron_values)-1):
-        # Normalized with neuron values derivatives
-        norm_input = net_input(weights[-thisLayer].transpose(),np.zeros(1),
-                               delta_values[-thisLayer],neuron_values[-thisLayer])
-        delta_values[-thisLayer-1] += norm_input
-    
-    return delta_values
-
-def weight_update(weights, biases, neuron_values, delta_values, learningRate):
-    delta_weights = [0]*(len(neuron_values)-1)
-    delta_biases = [0]*(len(neuron_values)-1)
-    # Weight update
-    for thisLayer in range(len(neuron_values)-1):
-        delta_weights[thisLayer] = np.multiply(learningRate, np.outer(neuron_values[thisLayer],delta_values[thisLayer]))
-        delta_biases[thisLayer] = np.multiply(learningRate, delta_values[thisLayer]) 
-        weights[thisLayer] += delta_weights[thisLayer]
-        biases[thisLayer] += delta_biases[thisLayer]
-        #weights[thisLayer] = weights[thisLayer].clip(min=-1,max=1)
-        #biases[thisLayer] = biases[thisLayer].clip(min=-1,max=1)
-        
-    return weights, biases, delta_weights, delta_biases
 
 def reset_neurons(neuron_values,delta_values):
+    # This is used to reset neuron value
     for thisLayer in range(len(neuron_values)):
         neuron_values[thisLayer].fill(0)
         delta_values[thisLayer-1].fill(0)
         
     return neuron_values, delta_values
-"""CONVOLUTIONAL NEURAL NETWORKS FUNCTIONALITY"""
-def fc_layer(up_neurons, down_neurons, weights, biases):
-    # We use the dot product to calculate the net input of every neuron,
-    # and subsequently add the bias vector
-    norm_input = net_input(weights, biases,up_neurons,up_neurons)
-    # The apply the activation functions to the net input
-    down_neurons += activation_function(norm_input)
-
-    return down_neurons
-
-def conv_layer(up_neurons, down_neurons, weights, biases, pad_width):
-    # In conv layers neurons are three dimensional tensors, exactly as weights
-    # and down neurons. We could put some constraints on the sizes but too expensive
-    ndimage.convolve
 
 """CALCULATE AND DISPLAY USEFUL INFORMATION"""
-def total_error_xentropy(delta_values,error_values):
-    #Returns the total error for every layer in the neural network
-    for i in range(len(delta_values)):
-        error_values[i] += np.sum(np.power(delta_values[i],2)) 
-    return error_values
-
 def total_error_MSE(delta_values,error_values):
     #Returns the total error for every layer in the neural network
     for i in range(len(delta_values)):
@@ -154,27 +272,3 @@ def total_error_MSE(delta_values,error_values):
 
 def print_training_info():
     return 0
-
-"""DICTIONARIES"""
-neuron_type_dictionary = {
-        'ReLU' : (ReLU_activation, ReLU_derivative),
-        'sigmoid' : (sigmoid_activation, sigmoid_derivative),
-        'tanh' : (tanh_activation, tanh_derivative),
-        'PWL' : (PWL_activation, PWL_derivative)
-        }
-
-(activation_function, neuron_derivative) = neuron_type_dictionary[NNvars.neuron_type]
-# LayerType dictionary
-layerType_dictionary = {
-        'CONV' : (),
-        'ReLU' : (),
-        'POOL' : (),
-        'FC' : ()
-        }
-# Overrides the previous definition of the backpropagation routine if the 
-# cross-entropy cost function is used instead of the MSE. 
-cost_function_dictionary = {
-        'MSE' : (backpropagation_MSE,total_error_MSE),
-        'xentropy' : (backpropagation_xentropy,total_error_xentropy)
-        }
-(backpropagation, total_error) = cost_function_dictionary[NNvars.cost_function]
--- NNvars.py
+++ NNvars.py
@@ -6,18 +6,13 @@ Here we correctly inizialize all the hyperparameters, neurons, synapses etc.
 @author: Alessandro Fumarola
 """
 import numpy as np
-
+import NNfuncs
 # System parameters for neural network, to be decided by the experimenter every
 # time
 # Used in the weight update function
 learningRate = 0.1
 # Every epoch, the learningrate will be decreased
 learningRateDecay = 1
-# Used in every activation function except for the ReLU
-# Sets the steepness of the activation functions
-slope_sigmoid = 10
-slope_tanh = 10
-slope_PWL = 10
 
 # Hyperparameters for training
 # How many images from MNIST?
@@ -35,11 +30,10 @@ initialization = 'uniform'
 # Topology of the network, put the number of neurons separated by a comma, as example
 # numNeurons = np.array([784,250,150,10])
 numNeurons = np.array([784,250,125,10])
-# Type of neurons, soon we'll support different types for different layers
-neuron_type = 'sigmoid'
+
 layerType = []
 layerType.append('input')
-layerType.extend([neuron_type] * (len(numNeurons)-2))
+layerType.extend([''])
 layerType.append('output')
 # Type of cost function
 cost_function = 'MSE'
@@ -49,10 +43,6 @@ cost_function = 'MSE'
 if len(layerType) != len(numNeurons):
     print('You must specify a layer type for every neuron layer!')
     print('By default, the network is set as fully connected with %s neurons ' % neuron_type)
-    layerType = []
-    layerType.append('input')
-    layerType.extend([neuron_type] * (len(numNeurons)-2))
-    layerType.append('output')
 
 # Information about training will be printed ~10 times for every Epoch
 numCheckIterations = np.round(numSamples/10).astype(np.int)
@@ -89,6 +79,16 @@ elif 'gaussian' in initialization:
     weights = [np.random.randn(numNeurons[i],numNeurons[i+1]) for i in range(len(numNeurons)-1)]
     biases = [np.random.randn(biasNeurons[i],numNeurons[i+1]) for i in range(len(numNeurons)-1)]
 
+"""DICTIONARIES"""
+# LayerType dictionary
+layerType_dictionary = {
+        'CONV' : (),
+        'ReLU' : (),
+        'POOL' : (),
+        'FC' : ()
+        }
+# Overrides the previous definition of the backpropagation routine if the 
+# cross-entropy cost function is used instead of the MSE. 
 print('SETUP for neural network:')
-print('learning rate = %f, learning rate decay = %f %%, neuron type = %s, number of input images = %d, epochs = %d' %
-      (learningRate,100*learningRateDecay, neuron_type, numSamples, numEpochs))
+print('learning rate = %f, learning rate decay = %f %%,  number of input images = %d, epochs = %d' %
+      (learningRate,100*learningRateDecay,numSamples, numEpochs))
